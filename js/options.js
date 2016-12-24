3b0419f4506e6a380728041a35d73bda20178214
Dan Bovey <dan@danbovey.uk>
2016-12-24 19:44:39 +0000

    Load storage on options page

--- js/options.js
+++ js/options.js
@@ -1,29 +1,31 @@
 const Storage = require('./storage');
 
-const options = Storage.get();
-const submit = document.querySelector('#save');
-const status = document.querySelector('#status');
-const inputs = {
-    base: document.querySelector('#base'),
-    key: document.querySelector('#key'),
-    poll: document.querySelector('#poll')
-};
+Storage.load()
+    .then(options => {
+        const submit = document.querySelector('#save');
+        const status = document.querySelector('#status');
+        const inputs = {
+            base: document.querySelector('#base'),
+            key: document.querySelector('#key'),
+            poll: document.querySelector('#poll')
+        };
 
-for(let name in inputs) {
-    inputs[name].value = options.api[name];
-}
+        for(let name in inputs) {
+            inputs[name].value = options.api[name];
+        }
 
-submit.addEventListener('click', e => {
-    for(let name in inputs) {
-        options.api[name] = inputs[name].value;
-    }
-    Storage.save(options)
-        .then(() => {
-            status.textContent = 'Options saved.';
+        submit.addEventListener('click', e => {
+            for(let name in inputs) {
+                options.api[name] = inputs[name].value;
+            }
+            Storage.save(options)
+                .then(() => {
+                    status.textContent = 'Options saved.';
 
-            window.setTimeout(() => {
-                tick.remove();
-            }, 1000);
+                    window.setTimeout(() => {
+                        tick.remove();
+                    }, 1000);
+                });
         });
-});
-document.body.appendChild(submit);
+        document.body.appendChild(submit);
+    });
